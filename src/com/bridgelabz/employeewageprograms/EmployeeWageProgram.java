77111fd9d1fed94bc1425c6c6ec10654b0e58072
sahana199835 <tnsahana1@gmail.com>
2023-03-23 15:21:06 +0530

    add:check total working hour

--- src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
+++ src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
@@ -5,39 +5,51 @@ public class EmployeeWageProgram {
 
 	public static void main(String[] args) {
 		
-		int wagePerHour = 20;
-        int fullDayHour = 8;
-        int partTimeHour = 4;
-        int workingDaysPerMonth = 20;
+		 int wagePerHour = 20;
+	        int fullDayHour = 8;
+	        int partTimeHour = 4;
+	        int workingDaysPerMonth = 20;
+	        int maxWorkingDays = 20;
+	        int maxWorkingHours = 100;
 
-        Scanner sc = new Scanner(System.in);
-        int hoursWorked;
-        int dailyWage = 0;
-        int monthlyWage = 0;
+	        Scanner input = new Scanner(System.in);
+	        int hoursWorked;
+	        int dailyWage = 0;
+	        int monthlyWage = 0;
+	        int totalWorkingHours = 0;
+	        int totalWorkingDays = 0;
 
-        for (int i = 1; i <= workingDaysPerMonth; i++) {
-            System.out.println("Enter the hours worked on day " + i + ": ");
-            hoursWorked = sc.nextInt();
+	        while (totalWorkingHours < maxWorkingHours && totalWorkingDays < maxWorkingDays){
+	            System.out.println("Enter the hours worked on day " + (totalWorkingDays + 1) + ": ");
+	            hoursWorked = input.nextInt();
 
-            switch(hoursWorked) {
-                case 8:
-                    dailyWage = wagePerHour * fullDayHour;
-                    break;
-                case 4:
-                    dailyWage = wagePerHour * partTimeHour;
-                    break;
-                default:
-                    System.out.println("Invalid hours worked");
-                    break;
-            }
+	            switch(hoursWorked) {
+	                case 8:
+	                    dailyWage = wagePerHour * fullDayHour;
+	                    break;
+	                case 4:
+	                    dailyWage = wagePerHour * partTimeHour;
+	                    break;
+	                default:
+	                    System.out.println("Invalid hours worked");
+	                    break;
+	            }
 
-            monthlyWage += dailyWage;
-        }
+	            monthlyWage += dailyWage;
+	            totalWorkingHours += hoursWorked;
+	            totalWorkingDays++;
+	        }
 
-        System.out.println("Monthly employee wage: $" + monthlyWage);
-        sc.close();
-	    }
+	        System.out.println("Total employee wage: $" + monthlyWage);
+	        System.out.println("Total working hours: " + totalWorkingHours);
+	        System.out.println("Total working days: " + totalWorkingDays);
+
+	        input.close();
 	}
+
+}
+
+        
 
 
 
