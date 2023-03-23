667cf0e2155f36332a42debfd387e8a44cf3d7ca
sahana199835 <tnsahana1@gmail.com>
2023-03-23 14:25:58 +0530

    add: .gitignore and README.md are files added

---
+++ src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewageprograms;
+
+public class EmployeeWageProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation programs");
+
+	}
+
+}
