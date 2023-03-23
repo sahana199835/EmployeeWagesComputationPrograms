cb6eadcd7d53cdef1ae468e4c2709cdd0199f776
sahana199835 <tnsahana1@gmail.com>
2023-03-23 15:04:20 +0530

    add:check employee parttime wages

--- src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
+++ src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
@@ -4,12 +4,25 @@ public class EmployeeWageProgram {
 
 	public static void main(String[] args) {
 		
-		int employeewageperhour = 20;
+		 int employeewageperhour = 20;
 		  int fulldayhours = 8;
-		  int dailyemployeewage = employeewageperhour * fulldayhours ;
-		  System.out.println("Daily Employee wage will be " +dailyemployeewage);
-		}
+		  int parttime = 4;
+		  int dailywage = 0;
+		 double empcheck = Math.floor(Math.random() * 10) %2;
 
+
+		 if (empcheck == 1) 
+				 {
+			       dailywage = employeewageperhour * fulldayhours;
+	               System.out.println("Employee is working as  fullday");
+				 }
+		 else 
+		 {
+	              dailywage = employeewageperhour * parttime;
+	              System.out.println("Employee is Part Time");
+	     }
+	              System.out.println("Daily Employee Wage: " + dailywage);
+	  }
 	}
 
 
