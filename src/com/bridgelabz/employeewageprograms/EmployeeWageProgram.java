f95b15db878fefd923501dd7f1aa74b6b64ef332
sahana199835 <tnsahana1@gmail.com>
2023-03-23 15:17:20 +0530

    add:check Wages for month

--- src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
+++ src/com/bridgelabz/employeewageprograms/EmployeeWageProgram.java
@@ -1,33 +1,43 @@
 package com.bridgelabz.employeewageprograms;
 
+import java.util.Scanner;
 public class EmployeeWageProgram {
 
 	public static void main(String[] args) {
 		
-		int employeewageperhour = 20;
-		int fulldayhours = 8;
-		int parttime = 4;
-		 int dailywage =0;
-		 int hours = 4;
-		 switch (hours) {
-            case 8:
-                dailywage = employeewageperhour * fulldayhours;
-                System.out.println("Employee fullday salary" +dailywage);
-                break;
-            case 4:
-                dailywage = employeewageperhour * parttime;
-                System.out.println("Employee parttime salary" +dailywage);
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                break;
-        } 
-	        System.out.println("Total salary: " +dailywage);
+		int wagePerHour = 20;
+        int fullDayHour = 8;
+        int partTimeHour = 4;
+        int workingDaysPerMonth = 20;
 
+        Scanner sc = new Scanner(System.in);
+        int hoursWorked;
+        int dailyWage = 0;
+        int monthlyWage = 0;
 
+        for (int i = 1; i <= workingDaysPerMonth; i++) {
+            System.out.println("Enter the hours worked on day " + i + ": ");
+            hoursWorked = sc.nextInt();
+
+            switch(hoursWorked) {
+                case 8:
+                    dailyWage = wagePerHour * fullDayHour;
+                    break;
+                case 4:
+                    dailyWage = wagePerHour * partTimeHour;
+                    break;
+                default:
+                    System.out.println("Invalid hours worked");
+                    break;
+            }
+
+            monthlyWage += dailyWage;
+        }
+
+        System.out.println("Monthly employee wage: $" + monthlyWage);
+        sc.close();
+	    }
 	}
 
-}
-	
 
 
